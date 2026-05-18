82a090434b8ce84c51f7cf3e9da9ca559593b2b0
KEVIN <karimboyevshaxram97@gmail.com>
2026-05-19 02:00:44 +0900

    feat: develop property like process within PropertyList screen component

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -10,10 +10,12 @@ import { PropertiesInquiry } from '../../libs/types/property/property.input';
 import { Property } from '../../libs/types/property/property';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
-import { Direction } from '../../libs/enums/common.enum';
-import { useQuery } from '@apollo/client';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_PROPERTIES } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
+import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -35,6 +37,8 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 	const [filterSortName, setFilterSortName] = useState('New');
 
     	/** APOLLO REQUESTS **/
+    const [ likeTargetProperty ] = useMutation(LIKE_TARGET_PROPERTY);
+
     const {
       loading: getPropertiesLoading,
       data: getPropertiesData,
@@ -80,6 +84,20 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 		);
 		setCurrentPage(value);
 	};
+
+         const likePropertyHandler = async (user: T, id: string) => {
+      try {
+        if (!id || !user?._id) throw new Error(Message.NOT_AUTHENTICATED);
+    
+        await likeTargetProperty({ variables: { input: id } });
+        await getPropertiesRefetch({ input: initialInput });
+        await sweetTopSmallSuccessAlert("success", 800);
+      } catch (err: any) {
+        console.log(err);
+        sweetMixinErrorAlert(err.message);
+      }
+    };
+    
 
 	const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
 		setAnchorEl(e.currentTarget);
@@ -163,7 +181,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 									</div>
 								) : (
 									properties.map((property: Property) => {
-										return <PropertyCard property={property} key={property?._id} />;
+										return <PropertyCard property={property} likePropertyHandler={likePropertyHandler} key={property?._id} />;
 									})
 								)}
 							</Stack>
@@ -199,7 +217,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 PropertyList.defaultProps = {
 	initialInput: {
 		page: 1,
-		limit: 9,
+		limit: 3,
 		sort: 'createdAt',
 		direction: 'DESC',
 		search: {
