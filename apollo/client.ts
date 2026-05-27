f75cc32e299750a15f216f1a91f2226e4d7d7b27
KEVIN <karimboyevshaxram97@gmail.com>
2026-05-27 18:05:31 +0900

    feat: admin related property control logic

--- apollo/client.ts
+++ apollo/client.ts
@@ -6,6 +6,7 @@ import { getMainDefinition } from '@apollo/client/utilities';
 import { onError } from '@apollo/client/link/error';
 import { getJwtToken } from '../libs/auth';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import { sweetErrorAlert } from '../libs/sweetAlert';
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 function getHeaders() {
